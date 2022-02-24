8b0ff9144d5a1eada941cfe6cd6b0db99f5e601d
Rob Sameh <rob.sameh@gmail.com>
2022-02-24 13:40:14 -0800

    wrote findTop() method for troubleshooting addTop()

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,42 @@
 //Business Logic
 
-function Pizza () {
+function Pizza (size) {
   this.toppings = {};
-  this.size = "small";
-  this.currentId = 0;
+  this.size = size;
+  this.topId = 0;
   this.totalCost = 0;
 }
 
-Pizza.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
+Pizza.prototype.assignTopId = function() {
+  this.topId += 1;
+  return this.topId;
 };
 
-Pizza.prototype.addTop = function(top) {
-  this.toppings[top.id] = top;
+Pizza.prototype.addTop = function(topping) {
+  this.topId = this.assignTopId();
+  this.toppings[topping.id] = topping;
 };
+
+Pizza.prototype.removeTop = function(top) {
+  if (this.toppings[top.id] === undefined) {
+    return false;
+  }
+  delete this.toppings[top.id];
+  return true;
+}
+
+Pizza.prototype.findTop = function(topId) {
+  if (this.toppings[topId] != undefined) {
+    return this.toppings[topId];
+  }
+  return false;
+}
 
 function Toppings(topping, cost){
   this.topping = topping;
   this.cost = cost;
 }
 
-let pizza = new Pizza();
+let pizza = new Pizza("small");
 let cheese = new Toppings("cheese", 1);
 let pep = new Toppings("Pepperoni", 2);
